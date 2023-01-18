dd1842ff9f05d0c302064dc209923d466cb998a7
Ankuristic <63995112+Ankuristic@users.noreply.github.com>
2023-01-18 18:57:25 +0530

    tooltip added

--- tooltip/src/App.js
+++ tooltip/src/App.js
@@ -1,14 +1,87 @@
-import React from 'react';
+import React  from 'react';
+import './App.css';
 import ToolTip from './ToolTip';
 
 
-function App() {
-  return (
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      position:"top",
+      hovering:false,
+    };
+  }
+  // handleclick which handle the position
+  handleClick=(pos)=>{
+    this.setState({
+      position:pos,
+      // console.log("pos",pos)
+    })
+  };
+// handle mouse enter
+  handleMouseEnter=()=>{
+    this.setState({
+      hovering:true,
+    });
+  };
+// handle mouse leave
+  handleMouseLeave=()=>{
+    this.setState({
+      hovering:false
+    })
+  }
+// render part of class component
+  render(){
+    return (
     <div className="App">
-      {/* <CartItem /> */}
-      <div> ToolTip </div>
-    </div>
-  );
+        <div className='button-position'>
+          <div className='group-name'>
+            select the position of the hovering!!
+          </div>
+          <button
+          className= {this.state.position=== 'top' ? "btn active-btn" :"btn" }
+          onClick={(e)=>{
+            this.handleClick('top');
+          }}>
+            top
+          </button>
+          <button
+          className={this.state.position=== 'left' ? "btn active-btn" :"btn"}
+          onClick={(e)=>{
+            this.handleClick('left');
+          }}>
+            left
+          </button>
+          <button
+          className={this.state.position=== 'right' ? "btn active-btn" :"btn"}
+          onClick={(e)=>{
+            this.handleClick('right');
+          }}>
+            right
+          </button>
+          <button
+          className={this.state.position=== 'down' ? "btn active-btn" :"btn"}
+          onClick={(e)=>{
+            this.handleClick('down');
+          }}>
+            down
+          </button>
+        </div>
+      <div id="button-container">
+          <button
+          className='btn hover-button'
+          onMouseOver={this.handleMouseEnter}
+          onMouseOut={this.handleMouseLeave}
+      >
+        Hover Over Me!!
+          </button>
+          {this.state.hovering && <ToolTip position={this.state.position}/>}
+        </div>
+      </div>
+    );
+  }
 }
 
+   
+       
 export default App;
--- tooltip/src/ToolTip.js
+++ tooltip/src/ToolTip.js
@@ -1,5 +1,17 @@
 import React from 'react';
+class ToolTip extends React.Component{
+    tooltip=()=>{
+        
+        return `${this.props.position}`;
+    }
+    render(){
+        return (
+            <div className={`${this.tooltip()}`}>
+                Thanks for hovering "{this.props.position}" 
+            </div>
+        )
+    }
+}
 
 
-
-// export default ToolTip;
+export default ToolTip;
